ae0c41b420c0d91d2fff686ac68afd8a6675e050
rsf2 <rustf2@gmail.com>
2013-11-20 19:17:52 +0000

    Added Flatly Theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -295,7 +295,11 @@ module.exports = function (grunt) {
 
     grunt.registerTask('server', function (target) {
         if (target === 'dist') {
-            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+            return grunt.task.run([
+              'build', 
+              //'open', 
+              'connect:dist:keepalive'
+            ]);
         }
 
         grunt.task.run([
